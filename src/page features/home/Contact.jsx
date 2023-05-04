621376cdc4830cb3e8f34fceab2dbdfd5788a7e1
Paul Okwulu <pauldevcodes@gmail.com>
2023-05-04 21:40:03 +0100

    contact section

--- src/page features/home/Contact.jsx
+++ src/page features/home/Contact.jsx
@@ -1,9 +1,20 @@
+import { styles } from "../../utils/styles";
+
 const Contact = () => {
-    return ( 
-        <div>
-            
+    return (
+        <div className={`${styles.paddingX} ${styles.paddingY}`}>
+            <div>
+                <div className=" flex flex-col gap-y-3 items-center mb-10 lg:mb-16">
+                    <h1 className={`${styles.sectionHeading}`}>
+                        Contact Us
+                    </h1>
+                    <p>
+                        Looking to get into Cybersecurity? Have questions about our class or need mentorship?
+                    </p>
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Contact;
