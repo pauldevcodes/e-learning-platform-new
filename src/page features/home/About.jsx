f00a44d36f82022e1f230b7cc59bfbc861b51588
Paul Okwulu <pauldevcodes@gmail.com>
2023-05-09 19:23:49 +0100

    about section

--- src/page features/home/About.jsx
+++ src/page features/home/About.jsx
@@ -1,5 +1,7 @@
+import { Link } from 'react-router-dom';
 import about from '../../images/AboutCompany.jpg'
 import { styles } from '../../utils/styles';
+import { ArrowRightIcon } from '@heroicons/react/24/solid';
 
 const About = () => {
     return (
@@ -13,15 +15,22 @@ const About = () => {
                     <div className=' basis-1/2'>
                         <img src={about} alt="" className='' />
                     </div>
-                    <p className=' text-center basis-1/2 lg:text-start'>
-                        Welcome to Kingship Technologies, your premier destination for cutting-edge cybersecurity training. Our mission is to empower individuals and organizations with the skills and knowledge required to safeguard their digital assets in an increasingly connected world.
-                        <br />
-                        At Kingship Technologies, we understand the continuously evolving nature of cybersecurity threats and the critical need for well-trained professionals to protect against them. That's why we've developed a comprehensive training program designed to equip you with the latest tools, techniques, and best practices in the field of cybersecurity.
-                        <br />
-                        Our team of experienced trainers boasts a diverse range of expertise in various aspects of cybersecurity, including Cybersecurity engineering, data privacy, risk management, and incident response. With years of combined experience, our instructors have honed their skills in both the public and private sectors, serving as security consultants, analysts and engineers. This wealth of practical experience allows us to provide real-world insights and deliver training that is both engaging and relevant.
-                        <br />
-                        At Kingship Technologies, we pride ourselves on offering a dynamic learning experience tailored to suit the needs of each individual participant. Our courses cater to a wide range of skill levels, from beginners looking to break into the field, to professionals seeking mentorship to get to the next level. We combine theoretical knowledge with hands-on training, ensuring that our students are not only well-versed in the principles of cybersecurity but also equipped to apply them in real-life scenarios.
-                    </p>
+                    <div className=' basis-1/2 flex flex-col gap-y-10'>
+                        <p className=' text-center lg:text-start'>
+                            Welcome to Kingship Technologies, your premier destination for cutting-edge cybersecurity training. Our mission is to empower individuals and organizations with the skills and knowledge required to safeguard their digital assets in an increasingly connected world.
+                            <br /><br />
+                            At Kingship Technologies, we understand the continuously evolving nature of cybersecurity threats and the critical need for well-trained professionals to protect against them. That's why we've developed a comprehensive training program designed to equip you with the latest tools, techniques, and best practices in the field of cybersecurity.
+                        </p>
+                        <Link
+                            to="about"
+                            className=" bg-orange-500 px-5 py-1 rounded-md w-fit text-white flex items-center gap-x-2 font-medium lg:text-lg"
+                        >
+                            Learn More
+                            <span>
+                                <ArrowRightIcon width={15} />
+                            </span>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
