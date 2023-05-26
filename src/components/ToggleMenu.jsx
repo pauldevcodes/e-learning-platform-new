edf34d01a4132318216fdb14d83e1db410ba5050
Paul Okwulu <pauldevcodes@gmail.com>
2023-05-27 00:55:24 +0100

    mobile error fixed

--- src/components/ToggleMenu.jsx
+++ src/components/ToggleMenu.jsx
@@ -87,7 +87,7 @@ const ToggleMenu = ({ isOpen, setIsOpen }) => {
                             })}
                         </ul>
                         <Link
-                            // to="register"
+                            to="register"
                             className=' font-["Poppins"] opacity-75 border-2 border-orange-500 rounded px-4 py-1 md:text-sm'
                         >
                             Register
